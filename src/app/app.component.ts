4248d850ecd5dd86193d646d5700ac31b5898caa
Manu <manuelnavarro89@gmail.com>
2020-05-26 22:58:55 +0200

    Testing API public call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "- My App -";
+
+  ninjaStats;
+
+  ngOnInit() {
+    fetch("https://fortnite-api.p.rapidapi.com/stats-alternative/ninja", {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "fortnite-api.p.rapidapi.com",
+        "x-rapidapi-key": "9265500cecmsh1c6f1103b2b0197p19cfeajsn325a1394646d",
+      },
+    })
+      .then(async (response: any) => {
+        this.ninjaStats = await response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
